9c73df131611d642219271618520364452e9a3c6
zakaria <loukilizakariae11@gmail.com>
2021-04-12 12:35:26 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { useState, useEffect } from "react";
-import Form from "./Form";
+import React, { useState } from "react";
+
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   const APIKEY = "3c6241ea411d041a680ec839e2d82f3d";
   const [weather, setWeather] = useState([]);
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
 
-  async function fetchdata(e) {
+  const clearInput = () => {
+    setCity("");
+    setCountry("");
+  };
+  async function fetchData(e) {
     e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
+
+    clearInput();
+
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${APIKEY}`
     );
@@ -24,8 +31,11 @@ function App() {
         Country: json.sys.country,
         temperature: Math.round(json.main.temp - 273.15), //La fonction Math.round() retourne la valeur d'un nombre arrondi à l'entier le plus proche.
         Description: json.weather[0].description,
-        Laltitude: json.coord.lat,
-        Longitude: json.coord.lon,
+
+        Pressure: json.main.pressure,
+        Humidity: json.main.humidity,
+        lat: json.coord.lat,
+        lon: json.coord.lon,
         error: "",
       });
     } else {
@@ -37,20 +47,46 @@ function App() {
     <div className="App">
       <header>
         {" "}
-        <h1 style={{ textAlign: "center" }}>Weather App</h1>
+        <h1 style={{ textAlign: "center" }}>Weather App ☁️</h1>
       </header>
 
-      <Form getweather={fetchdata} />
+      <div className="form">
+        <form onSubmit={fetchData} className="searchbox">
+          <input
+            type="text"
+            name="city"
+            value={city}
+            placeholder="Tap a city"
+            className="searchbox-bar"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <input
+            type="text"
+            name="country"
+            value={country}
+            placeholder="Tap a country"
+            className="searchbox-bar"
+            onChange={(e) => setCountry(e.target.value)}
+          />
+          <button className="searchbox-box">Submit</button>
+        </form>
+      </div>
 
       <Weather
         City={weather.City}
         Country={weather.Country}
         temperature={weather.temperature}
         Description={weather.Description}
-        Laltitude={weather.Laltitude}
-        Longitude={weather.Longitude}
+        Pressure={weather.Pressure}
+        Humidity={weather.Humidity}
+        lat={weather.lat}
+        lon={weather.lon}
         error={weather.error}
       />
+      <div id="footer">
+        © Copyright 2021. LOUKILIZAKARIA All Rights Reserved.{" "}
+        <a>Mentions légales</a> | <a> Politique de confidentialité</a>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,12 +5,35 @@ const Weather = ({
   Country,
   temperature,
   Description,
-  Laltitude,
-  Longitude,
+  lon,
+  lat,
+  Humidity,
+  Pressure,
 }) => {
   const ImageCloud = (
     <img
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSY4zTz5NjNrF8sMS_rmzU393yvzBSjWS99Qg1_NvB2hwKRkTw_&usqp=CAU"
+      src="https://images.twinkl.co.uk/tr/image/upload/illustation/Strong-Winds-Symbol---Geography-Coasts-Weather-Cloud-KS3.png"
+      width="50%"
+      height="50%"
+    />
+  );
+  const clearSky = (
+    <img
+      src="https://images.twinkl.co.uk/tr/image/upload/illustation/Sun---Rain-Cloud-Weather-Phenomena-Light-Water-Cycle-KS1.png"
+      width="50%"
+      height="50%"
+    />
+  );
+  const rainImage = (
+    <img
+      src="https://images.twinkl.co.uk/tr/image/upload/illustation/Heavy-Rain-Symbol---Geography-Coasts-Weather-Cloud-KS3.png"
+      width="50%"
+      height="50%"
+    />
+  );
+  const snowImage = (
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE5rPxha9IP_D7DP1kc-uoNHAzYS-qZtp93_5vTXyPSbnQOXLHM4w_cIEFHsyiaMRHN1Y&usqp=CAU"
       width="50%"
       height="50%"
     />
@@ -20,14 +43,30 @@ const Weather = ({
     if (Description) {
       const WeatherDescription = Description.split(" ");
       const cloudWeather = ["cloud", "cloudy", "clouds", "overcast"];
+      const cloudWeatherSky = ["clear", "clear sky", "sky"];
+      const cloudWeatherRain = [
+        "rainy",
+        "rain",
+        "light rain",
+        "rains",
+        "moderate rain",
+      ];
+      const cloudWeatherSnow = ["snow", "snowy"];
       for (var i = 0; i < WeatherDescription.length; i++) {
         if (cloudWeather.includes(WeatherDescription[i])) {
           return ImageCloud;
-        }
+        } else if (cloudWeatherSky.includes(WeatherDescription[i])) {
+          return clearSky;
+        } else if (cloudWeatherRain.includes(WeatherDescription[i])) {
+          return rainImage;
+        } else if (cloudWeatherSnow.includes(WeatherDescription[i])) {
+          return snowImage;
+        } else console.log(WeatherDescription);
       }
       console.log("Description is " + WeatherDescription);
     }
   }
+  console.log(Description);
   {
     /*const str = "Être ou ne pas être, telle est la question.";
 
@@ -35,21 +74,86 @@ console.log(str.includes("Être"));       // true
 console.log(str.includes("question"));   // true
 console.log(str.includes("pléonasme"));  // false*/
   }
-  return (
-    <div className="Weather">
-      {City && Country && (
-        <p>
-          Ville:{City},Pays:{Country}
-        </p>
-      )}
 
-      {temperature && <p>Temperature:{temperature}°C</p>}
-      {Description && <p>Description:{Description}</p>}
-      {Longitude && <p>Longitude{Longitude}λ</p>}
-      {Laltitude && <p>Laltitude{Laltitude}φ</p>}
-      {Description && Decor()}
-    </div>
-  );
+  if (!City) {
+    return (
+      <>
+        <div className="home">
+          <img
+            src="https://i.pinimg.com/originals/06/c4/f7/06c4f70ec5931e2342e703e8a3f0a253.png"
+            width="20%"
+            height="20%"
+          />
+
+          <p>Hello there ! Welcome , Search for a city </p>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div className="weath">
+        <div className="Weather">
+          <div className="img"> {Description && Decor()}</div>
+          {City && Country && (
+            <p>
+              <span> City :</span>
+              {City},<span> Country :</span>
+              {Country}
+            </p>
+          )}
+
+          {temperature > 10 ? (
+            <p>
+              <span>Temperature in °C: </span>
+              {temperature}{" "}
+              <i class="fas fa-temperature-high" style={{ color: "red" }}></i>
+            </p>
+          ) : (
+            <p>
+              <span>Temperature in °C : </span>
+              {temperature}{" "}
+              <i
+                class="	fas fa-temperature-low"
+                style={{ color: "darkcyan" }}
+              ></i>
+            </p>
+          )}
+
+          {Description && (
+            <p style={{ textTransform: "capitalize" }}>
+              <span>Description:</span>
+              {Description}
+            </p>
+          )}
+          {Pressure && (
+            <p>
+              <span>Pressure :</span>
+              {Pressure} hPa
+            </p>
+          )}
+          {Humidity && (
+            <p>
+              <span>Humidity : </span>
+              {Humidity} % <i class="fas fa-humidity"></i>
+            </p>
+          )}
+          {lon && (
+            <div>
+              <span>Geographic Coordinates :</span>
+              <p>
+                <span>Latitude:</span>
+                {lat} φ
+              </p>
+              <p>
+                <span>Longitude :</span>
+                {lon} λ
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Weather;
